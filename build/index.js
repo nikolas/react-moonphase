949510964a6190d126caefd2758929f01d7983b8
Nikolas Nyby <nikolas@gnu.org>
2018-10-10 19:48:06 -0400

    update build

--- build/index.js
+++ build/index.js
@@ -5,6 +5,4 @@ var _MoonPhaseView = _interopRequireDefault(require("./MoonPhaseView"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /* eslint-env node */
-module.exports = {
-  MoonPhaseView: _MoonPhaseView.default
-};
+module.exports = _MoonPhaseView.default;
